7dba01babdd267d201f483c433dc9c7f77c02772
Sowmiyadevi44 <sowmisaran2@gmail.com>
2017-06-07 15:13:13 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class number
+{
+public void main(String args[])
+{
+int n;
+Scanner scan=new scan(System.in);
+n=scan.nextInt();
+if(n==0)
+System.out.println(n+"is zero");
+else
+if(n%2==0)
+System.out.println(n+"is even");
+else
+System.out.println(n+"is odd");
+}
+}
